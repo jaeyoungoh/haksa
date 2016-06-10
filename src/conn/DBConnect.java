d2e3a2693896c0a44366d6683df3b908ac4effc4
오재영 <wodud.oh@gmail.com>
2016-06-10 15:18:23 +0900

    DB 설정

---
+++ src/conn/DBConnect.java
@@ -0,0 +1,35 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+	
+	private static DBConnect db = new DBConnect();
+	private Connection conn = null;
+	String jdbc_driver = "oracle.jdbc.driver.OracleDriver";
+	String jdbc_url = "jdbc:oracle:thin:@192.168.14.31:1521:xe";
+	
+	private DBConnect() {
+		try {
+			Class.forName(jdbc_driver);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DBConnect getInstance() {
+		return db;
+	}
+	public Connection getConnection(){
+		try {
+			conn = DriverManager.getConnection(jdbc_url, "kitri04", "kitri04");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
